801d12e8c385476351a4dfd128e31f88b71f45b8
oriana13 <oriana.pfalz13@gmail.com>
2022-01-04 16:33:00 +0100

    library error

---
+++ Include/error.h
@@ -0,0 +1,15 @@
+//
+// Created by crichard23 on 23/10/2021.
+//
+
+#ifndef _WORLD_ERROR_H_
+# define _WORLD_ERROR_H_
+
+# include <stdio.h>
+# include <time.h>
+
+# define __PATH_ERROR_FILE__ "./../log/log.txt"
+
+void errorLog(const char *messageError);
+
+#endif
