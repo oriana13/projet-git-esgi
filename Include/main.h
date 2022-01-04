119bdd97664def8324e93ec0dd27578d7f25fcf9
oriana13 <oriana.pfalz13@gmail.com>
2022-01-04 16:29:26 +0100

    library general

---
+++ Include/main.h
@@ -0,0 +1,16 @@
+//
+// Created by crichard23 on 15/10/2021.
+//
+
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <string.h>
+#include "inventory.h"
+
+
+
+#endif //_MAIN_H_
+
